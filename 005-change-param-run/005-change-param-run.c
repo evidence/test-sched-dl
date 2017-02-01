6fbd0cfe70be194bd5ae7a998d193c0a1e9b923a
Claudio Scordino <claudio@evidence.eu.com>
2017-02-01 09:45:53 +0100

    Tests 004 and 005

---
+++ 005-change-param-run/005-change-param-run.c
@@ -0,0 +1,54 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include "sched_utils.h"
+
+void *periodic_change(void* param)
+{
+	printf("Second pid = %d", getpid());
+        int pid = *((int*) param);
+
+        for (int i = 1;; ++i) {
+                unsigned int flags = 0;
+                struct sched_attr attr;
+                attr.size = sizeof(attr);
+                attr.sched_flags = 0;
+                attr.sched_nice = 0;
+                attr.sched_priority = 0;
+                attr.sched_policy = SCHED_DEADLINE;
+                attr.sched_runtime = (30 * 1000 * 1000)/i;
+                attr.sched_period = attr.sched_deadline = 40 * 1000 * 1000;
+                if (sched_setattr(pid, &attr, flags) < 0)
+                        perror("sched_setattr()");
+                sleep(1);
+        }
+        return NULL;
+}
+
+
+
+int main (void)
+{
+        pthread_t tid;
+        int pid = getpid();
+        pthread_create(&tid, NULL, periodic_change, (void *) &pid);
+	printf("Main pid = %d", pid);
+	unsigned int flags = 0;
+	struct sched_attr attr;
+	attr.size = sizeof(attr);
+        attr.sched_flags = 0;
+        attr.sched_nice = 0;
+        attr.sched_priority = 0;
+
+       /* This creates a 10ms/30ms reservation */
+        attr.sched_policy = SCHED_DEADLINE;
+        attr.sched_runtime = 10 * 1000 * 1000;
+        attr.sched_period = attr.sched_deadline = 30 * 1000 * 1000;
+
+        if (sched_setattr(0, &attr, flags) < 0)
+                perror("sched_setattr()");
+	else
+		for (;;);
+	return 0;
+}
