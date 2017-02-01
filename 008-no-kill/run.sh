94d14b1aba26bb0dfd622e1277c6ce0005a252c2
Claudio Scordino <claudio@evidence.eu.com>
2017-02-01 10:23:39 +0100

    Test 008

---
+++ 008-no-kill/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+make clean_data
+DIR=`basename $PWD`
+if [ ! -e $DIR ]; then
+	echo "ERROR: File not compiled! Type make"
+	exit
+fi
+trace-cmd reset
+echo "Running test $DIR..."
+dmesg -c > /dev/null
+trace-cmd record -a -r 90 -b 100000 -e sched -o trace.dat ./$DIR
+## sleep 10
+## echo "Killing test $DIR..."
+## killall $DIR > /dev/null
+dmesg -c > ./dmesg.txt
+cat dmesg.txt
+
