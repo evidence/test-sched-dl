8f6aa21fc4cb0d0c23fe123ad5cc1c63c79ac6a6
Claudio Scordino <claudio@evidence.eu.com>
2017-02-20 09:25:39 +0100

    T0008: test cfs->dl when sleeping

---
+++ T0008-through-cfs-sleep/main.c
@@ -0,0 +1,48 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include "sched_utils.h"
+
+int flag = 0;
+
+void *periodic_change(void* param)
+{
+        int pid = *((int*) param);
+        for (int i = 0;; ++i) {
+                unsigned int flags = 0;
+                struct sched_attr attr;
+                attr.size = sizeof(attr);
+                attr.sched_nice = 0;
+		attr.sched_flags = flag;
+                attr.sched_priority = 0;
+                if (i%2){
+                        /* This creates a 10ms/30ms reservation */
+                        attr.sched_policy = SCHED_DEADLINE;
+                        attr.sched_runtime = 30 * 1000 * 1000;
+                        attr.sched_period = attr.sched_deadline = 40 * 1000 * 1000;
+                } else {
+                        attr.sched_policy = SCHED_OTHER;
+                }
+                if (sched_setattr(pid, &attr, flags) < 0)
+                        perror("sched_setattr()");
+                sleep(1);
+        }
+        return NULL;
+}
+
+
+int main (int argc, char *argv[])
+{
+	if (argc > 1)
+		flag = atoi(argv[1]);
+        pthread_t tid;
+        int pid = getpid();
+        pthread_create(&tid, NULL, periodic_change, (void *) &pid);
+
+        sleep(100);
+        return 0;
+}
+
+
+
