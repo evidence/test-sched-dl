cac977d1c9d90ec1a1cdd4167789a873c16389f5
Claudio Scordino <claudio@evidence.eu.com>
2017-02-23 14:55:17 +0000

    check.sh: indent dmesg output

--- check.sh
+++ check.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-## Check if we have root permissions
-
 for TEST in `ls -d T0* | xargs -r`; do
+	echo "==================================="
 	echo "==================================="
 	echo "Checking directory $TEST"
 	cd $TEST
 	cat dmesg.txt
+        echo "-----------------------------------"
+        echo "-----------------------------------"
+        sleep 1
 	kernelshark ./trace.dat
 	cd ..
 done
