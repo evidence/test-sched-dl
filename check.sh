15f560fb79969c290bc5457039d1bac9f153745a
Claudio Scordino <claudio@evidence.eu.com>
2017-02-23 15:38:43 +0000

    check.sh: allow checking a specific test

--- check.sh
+++ check.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
-for TEST in `ls -d T0* | xargs -r`; do
+
+do_check() {
 	echo "==================================="
 	echo "==================================="
-	echo "Checking directory $TEST"
-	cd $TEST
+	echo "Checking directory $1"
+	cd $1
 	cat dmesg.txt
         echo "-----------------------------------"
         echo "-----------------------------------"
         sleep 1
 	kernelshark ./trace.dat
 	cd ..
-done
+}
+
+
+
+if [[ $1 == "" ]];then
+	for TEST in `ls -d T0* | xargs -r`; do
+		do_check $TEST
+	done
+else
+	do_check $1
+fi
